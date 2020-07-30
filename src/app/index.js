dee944707eb43d0644b0ea4d6184242aee33c278
unknown <maxym.tymoshenko@quartesian.com>
2020-07-30 15:37:05 +0300

    initial commit

---
+++ src/app/index.js
@@ -0,0 +1,10 @@
+//src/app/index.js
+import angular from 'angular';
+import template from './index.tpl.html';
+import '../style/app.scss';
+
+let component = {
+    template // Use ES6 enhanced object literals.
+}
+let app = angular.module('app', [])
+    .component('app', component)
